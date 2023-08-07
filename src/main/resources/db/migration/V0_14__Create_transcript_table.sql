2beb447532082d462caabec2708aa0c8c029e0f0
kenny <hei.kenny.3@gmail.com>
2023-08-07 11:22:36 +0300

    feat : specs and migration

---
+++ src/main/resources/db/migration/V0_14__Create_transcript_table.sql
@@ -0,0 +1,8 @@
+create table "transcript"(
+                             id varchar primary key,
+                             student_id varchar references "user"(id),
+                             semester varchar not null,
+                             academic_year varchar,
+                             is_definitive boolean,
+                             creation_datetime timestamp with time zone not null default now()
+);
---
+++ src/main/resources/db/migration/V0_15__Create_version_table.sql
@@ -0,0 +1,7 @@
+create table "version"(
+                          id varchar primary key,
+                          ref int not null,
+                          created_by varchar references  "user"(id),
+                          transcript_id varchar references "transcript"(id),
+                          creation_datetime timestamp with time zone not null default now()
+);
